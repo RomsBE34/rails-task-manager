89f08ec262ae2c68ff429e46234b897ec497c085
Romain Blanco <romain.blanco@gmail.com>
2019-02-08 16:19:33 +0800

    add fonction added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,26 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+
+  def create
+     @task = Task.new(task_params)
+     @task.save
+     redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def task_params
+      params.require(:task).permit(:title, :detail)
+  end
 end
+
